efad8f29f51a84d42f2ce38c0515c3bc0fce695e
cranpun <cranpun@gmail.com>
2018-10-01 16:38:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# translate english japanese
+
+## before use...
+
+1. make api on google app script
+1. create .env from .env.sample
+
+## how to use
+
+### en -> ja
+
+```commandline
+node /path/to/main.js en sentence
+```
+
+### ja -> en
+
+```commandline
+node /path/to/main.js ja sentence
+```
---
+++ main.js
@@ -0,0 +1,55 @@
+const axios = require("axios");
+const qs = require("qs");
+
+require("dotenv").config();
+
+const trans = (word, source, target) => {
+    const url = process.env.API_URL;
+    const params = qs.stringify({
+        'text': word,
+        'source': source,
+        'target': target
+    });
+    const headers = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    };
+
+    axios
+        .post(url, params, headers)
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+const usage = function (mes) {
+    console.log(mes);
+    console.log("\t en -> ja: : node main.js ja word");
+    console.log("\t ja -> en: : node main.js en 単語");
+}
+
+if (process.argv.length < 4) {
+    usage("Unexpected args");
+    return;
+}
+
+const tgt = process.argv[2];
+let src = "en";
+if (tgt === "en") {
+    src = "ja";
+} else if(tgt === "ja") {
+    src = "en";
+} else {
+    usage("Unexpected mode");
+    return;
+}
+
+let words = [];
+for(let i = 3; i < process.argv.length; i++) {
+    words.push(process.argv[i]);
+}
+const text = words.join(" ");
+trans(text, src, tgt);
